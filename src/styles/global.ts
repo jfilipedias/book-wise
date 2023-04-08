0ea2d4d36f483654090a40dcd750de504b4e38f5
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-04-08 19:08:40 -0300

    feat: Setups stitches styles

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { globalCss } from './stitches.config'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    margin: 0,
+    padding: 0,
+  },
+
+  body: {
+    backgroundColor: '$gray800',
+    color: '$gray100',
+    '--webkit-font-smoothing': 'antialiased',
+  },
+})
