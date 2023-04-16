6d0c8d60c3ee96f870fe674d9fbaf7964ab9ccd0
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-04-16 11:38:40 -0300

    refactor: Renames Login route

---
+++ src/pages/login/index.page.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import GoogleIcon from '@/assets/icons/google-icon.svg'
+import GitHubIcon from '@/assets/icons/github-icon.svg'
+import RocketIcon from '@/assets/icons/rocket-icon.svg'
+import {
+  Button,
+  Container,
+  Hero,
+  LoginContainer,
+  LoginOptions,
+  WelcomeContainer,
+} from './styles'
+
+export default function Login() {
+  return (
+    <Container>
+      <Hero>
+        <Image
+          src="/images/hero.png"
+          alt="Imagem de uma pessoa loira deitada sobre almofadas em um sofá enquanto lê um livro"
+          quality={100}
+          fill
+        />
+      </Hero>
+
+      <LoginContainer>
+        <WelcomeContainer>
+          <h1>Boas Vindas!</h1>
+          <span>Faça seu login ou acesse como visitante.</span>
+        </WelcomeContainer>
+
+        <LoginOptions>
+          <Button>
+            <Image src={GoogleIcon} alt="Icone do Google" />
+            Entrar com o Google
+          </Button>
+
+          <Button>
+            <Image src={GitHubIcon} alt="Icone do GitHub" />
+            Entrar com o GitHub
+          </Button>
+
+          <Button>
+            <Image src={RocketIcon} alt="Icone de foguete" />
+            Acessar como visitante
+          </Button>
+        </LoginOptions>
+      </LoginContainer>
+    </Container>
+  )
+}
