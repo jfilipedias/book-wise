f94d4eda546cf3209141b0bc5beb5927bd3d1fce
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-04-18 23:55:57 -0300

    chore: Setups remote images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
