4b7c02a5f5364626315b9608bf17a6d0aedf484c
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-04-18 23:55:40 -0300

    feat: Setups axios and react query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
 import { Nunito_Sans } from 'next/font/google'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { queryClient } from '@/lib/react-query'
 import { globalStyles } from '@/styles/global'
 
 const nunitoSans = Nunito_Sans({ subsets: ['latin'], weight: ['400', '700'] })
@@ -8,8 +11,12 @@ globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={nunitoSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className={nunitoSans.className}>
+        <Component {...pageProps} />
+      </div>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
