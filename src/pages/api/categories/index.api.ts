1b511408dfa3c153228fd665f8b847de415721a8
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-04-23 15:33:01 -0300

    feat: Creates the categories api route

---
+++ src/pages/api/categories/index.api.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const categories = await prisma.category.findMany()
+
+  return res.status(200).json(categories)
+}
