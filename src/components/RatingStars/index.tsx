8b34087dcd5a248114f2e433c8202e5fd0b9c8f7
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-05-15 23:36:02 -0300

    styles: Updates the RatingStars styles

--- src/components/RatingStars/index.tsx
+++ src/components/RatingStars/index.tsx
@@ -5,12 +5,7 @@ export function RatingStars({ rate }: { rate: number }) {
   return (
     <Container>
       {Array.from({ length: 5 }, (_, i) => i + 1).map((index) => (
-        <Star
-          key={index}
-          size={16}
-          color="#8381D9"
-          weight={rate >= index ? 'fill' : 'regular'}
-        />
+        <Star key={index} weight={rate >= index ? 'fill' : 'regular'} />
       ))}
     </Container>
   )
--- src/components/RatingStars/styles.ts
+++ src/components/RatingStars/styles.ts
@@ -3,4 +3,9 @@ import { styled } from '@/styles/stitches.config'
 export const Container = styled('div', {
   display: 'flex',
   gap: '$1',
+  svg: {
+    color: '$purple100',
+    width: 16,
+    height: 16,
+  },
 })
